661117b2673e0dbb9647b7f15b9813408a57d98f
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-08-06 12:00:20 +0100

    dim microbit display for filming

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
 modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
     basic.showNumber(2)
 })
+led.setBrightness(64)
 basic.showIcon(IconNames.Diamond)
 basic.forever(function () {
 	
