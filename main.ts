0d483a0433f85d43044b300c07a91a77747352e8
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-08-06 11:38:26 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
+    basic.showNumber(1)
+})
+modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
+    basic.showNumber(2)
+})
+basic.showIcon(IconNames.Diamond)
+basic.forever(function () {
+	
+})
